b8f8cf7e0cd75b784f854f651ef145cdc3bce085
Rob Sandalls <robert.sandalls@intagrange.com>
2015-04-07 22:40:20 +0100

    Work
    Upload Plots 1 & 2

---
+++ plot1.R
@@ -0,0 +1,5 @@
+testdata <- read.table("household_power_consumption.txt", sep=";", header=TRUE)
+outdata <- subset(testdata, Date=="1/2/2007"|Date=="2/2/2007")
+png("plot1.png", width = 480, height = 480)
+hist(as.numeric(outdata$Global_active_power)*2/1000,xlab="Global Active Power (kilowatts)",col="red",main="Global Active Power")
+dev.off()
